aed97796d8e764d3c03dde729c48c536bc45f894
Jared R. Mallas <jrtytn@gmail.com>
2018-09-13 19:24:27 -0500

    initial branch commit

--- src/org/jrm/Main.java
+++ src/org/jrm/Main.java
@@ -2,7 +2,8 @@ package org.jrm;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	// initial commit for branch
     }
 }
