cae1295c9f5ed61f42a863ba4041c7aa5e8e7f14
Jared R. Mallas <jrtytn@gmail.com>
2018-09-17 07:52:05 -0700

    add fileIO with tests

--- src/org/jrm/Main.java
+++ src/org/jrm/Main.java
@@ -1,9 +1,46 @@
 package org.jrm;
 
+import org.jrm.io.FileInput;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
-	// initial commit for branch
+    private final static FileInput indata = new FileInput("the_book.csv");
+    private final static Map<String, Integer> map = new HashMap<String, Integer>();
+
+    public static void main(String[] args) {
+        String line;
+        String[] words;
+        Object wordFound;
+        //   String[] fields;
+
+        while ((line = indata.readLine()) != null) {
+            line=line.replace(",","").replace(".","")
+                    .replace(";","").replace(":","")
+                    .replace("'","").replace("\"","")
+                    .replace("-","").replace("!","")
+                    .replace("#","").replace("(","")
+                    .replace(")","").replace("?","")
+                    .replace("_"," ").replace("?","")
+                    .toLowerCase().trim();
+            words = line.split(" ");
+            for (String s:words) {
+                wordFound = map.get(s);
+                if (wordFound == null) {
+                    map.put(s, new Integer(1));
+                }
+                else {
+                    map.put(s, map.get(s) + 1);
+                }
+
+            }
+
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+        }
     }
+
 }
